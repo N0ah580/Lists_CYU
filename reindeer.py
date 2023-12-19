5e37e03ebca89d979181ee41dc8aa4184788ff80
N0ah580 <145355575+N0ah580@users.noreply.github.com>
2023-12-19 08:32:06 -0500

    FUCK THIS

--- reindeer.py
+++ reindeer.py
@@ -2,5 +2,5 @@
 # Description: Reindeer
 
 #create list of santa's reindeer
-santas_reindeer = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comit", "Cupid", "Donnor", "Blitzen"]
+santas_reindeer = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comit", "Cupid", "Donder", "Blitzen"]
 
